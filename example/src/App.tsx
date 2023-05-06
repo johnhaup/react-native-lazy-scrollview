f7937194a7b374f3fa3511f0e8545c84e69401fd
John Haupenthal <johnhaup@gmail.com>
2023-05-05 19:33:42 -0700

    Remove context dep (#10)
    * fix: fix example app
    
    * chore: make example better
    
    * feat: allow scrollview props to pass to LazyScrollView

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,57 +1,77 @@
-import React, { useState } from 'react';
-import { Dimensions, SafeAreaView, Text, View } from 'react-native';
-import { LazyChild, LazyScrollView } from 'react-native-lazy-scrollview';
+import React from 'react';
 
-type Child = {
-  name: string;
-  height: number;
-  backgroundColor: string;
-};
+import { Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { LazyScrollView } from 'react-native-lazy-scrollview';
+import { ColorBlock } from './components/ColorBlock';
+import shuffle from 'lodash/shuffle';
 
-const COMPS = [
-  { name: 'one', height: 400, backgroundColor: 'red' },
-  { name: 'two', height: 800, backgroundColor: 'blue' },
-  { name: 'three', height: 200, backgroundColor: 'yellow' },
-  { name: 'four', height: 500, backgroundColor: 'green' },
-  { name: 'five', height: 400, backgroundColor: 'pink' },
+const ALBUMS = [
+  'https://audioxide.com/api/images/album-artwork/in-utero-nirvana-medium-square.jpg',
+  'https://audioxide.com/api/images/album-artwork/pinkerton-weezer-medium-square.jpg',
+  'https://static1.squarespace.com/static/55b7b1bde4b0828f7d1438de/t/6091766fa1c5b50917511c4b/1620145812302/WAVVES+-+Hideaway+-+Album+Cover+3000x3000.jpg?format=1500w',
+  'https://i.scdn.co/image/ab67616d0000b27313f2466b83507515291acce4',
+  'https://m.media-amazon.com/images/I/515NY3NS1EL._UF1000,1000_QL80_.jpg',
+  'https://images.squarespace-cdn.com/content/v1/5e270e203c421657bd3e3208/1584536470858-1OSWN7WUQ3DY749LGHOU/R-1843384-1282324049.jpeg.jpg',
+  null,
+  'https://m.media-amazon.com/images/I/61vMlYT58HL._UF1000,1000_QL80_.jpg',
+  'https://e.snmc.io/i/1200/s/9a0f51b8b776171aaee65c1a352f6d11/2396429',
+  'https://global-uploads.webflow.com/5fda4244c42e015b06d2fd8d/5fdbca8f15a8ee0f0f369767_cover.jpg',
+  'https://i.discogs.com/GXyIsX5gBpgw3XMtEZ-FvyVJBDZyk7Aoq-bPzV_yIXk/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTIyNDY1/MTUtMTI5OTE5MjI1/NS5qcGVn.jpeg',
+  null,
 ];
 
-function ColorBlock({
-  name,
-  height: injectHeight,
-  backgroundColor: injected,
-}: Child) {
-  const [backgroundColor, setBgColor] = useState(injected);
-  const [height, setHeight] = useState(injectHeight);
+const OFFSETS = [-100, -500];
 
-  const onThresholdPass = () => {
-    setBgColor('gray');
-    setHeight(injectHeight * 3);
-  };
+export default function App() {
+  const renderBlock = (uri: string | null, i: number) => (
+    <ColorBlock key={`child_${i}`} uri={uri} />
+  );
+
+  const renderScrollView = (offset: number) => (
+    <View key={`${offset}`} style={styles.scrollviewContainer}>
+      <LazyScrollView
+        contentContainerStyle={styles.scrollview}
+        offset={offset}
+        showsVerticalScrollIndicator={false}
+      >
+        {shuffle(ALBUMS).map(renderBlock)}
+      </LazyScrollView>
+      <View style={styles.offsetBar}>
+        <Text style={styles.offsetText}>{`Offset ${offset}`}</Text>
+      </View>
+    </View>
+  );
 
   return (
-    <LazyChild onThresholdPass={onThresholdPass}>
-      <View
-        style={{
-          width: Dimensions.get('screen').width,
-          height,
-          backgroundColor,
-        }}
-      >
-        <Text>{name}</Text>
-      </View>
-    </LazyChild>
+    <ScrollView horizontal pagingEnabled>
+      {OFFSETS.map(renderScrollView)}
+    </ScrollView>
   );
 }
 
-export const App = () => {
-  const renderItem = (props: Child, index: number) => {
-    return <ColorBlock key={`anim-child-${index}`} {...props} />;
-  };
-
-  return (
-    <SafeAreaView>
-      <LazyScrollView offset={-400}>{COMPS.map(renderItem)}</LazyScrollView>
-    </SafeAreaView>
-  );
-};
+const styles = StyleSheet.create({
+  scrollviewContainer: {
+    height: Dimensions.get('screen').height,
+    width: Dimensions.get('screen').width * 0.5,
+    paddingHorizontal: 8,
+  },
+  scrollview: {
+    paddingVertical: 40,
+  },
+  offsetBar: {
+    position: 'absolute',
+    top: 100,
+    left: 0,
+    right: 0,
+    backgroundColor: '#000',
+    opacity: 0.7,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  offsetText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: '600',
+  },
+});
