2455b0cda8619649ef82d27709ef69e8ae999435
John Haupenthal <johnhaup@gmail.com>
2023-05-07 09:57:59 -0700

    test: update tests to include onEnd trigger

--- __tests__/components/LazyChild.test.tsx
+++ __tests__/components/LazyChild.test.tsx
@@ -14,22 +14,14 @@ import { LazyScrollView } from '../../src/components/LazyScrollView';
 describe('LazyChild', () => {
   const onThresholdPassOne = jest.fn();
   const onThresholdPassTwo = jest.fn();
-  const CHILD_HEIGHT = 400;
-  const SCROLLVIEW_HEIGHT = 300;
+  const onThresholdPassThree = jest.fn();
+  const HEIGHT = 400;
+  const SMALL_HEIGHT = 100;
+  const CONTENT_HEIGHT = HEIGHT * 2 + SMALL_HEIGHT;
 
-  function ChildOne() {
+  function Child({ onThresholdPass }: { onThresholdPass: () => void }) {
     return (
-      <LazyChild onThresholdPass={onThresholdPassOne}>
-        <View>
-          <Text>I am a child</Text>
-        </View>
-      </LazyChild>
-    );
-  }
-
-  function ChildTwo() {
-    return (
-      <LazyChild onThresholdPass={onThresholdPassTwo}>
+      <LazyChild onThresholdPass={onThresholdPass}>
         <View>
           <Text>I am a child</Text>
         </View>
@@ -40,8 +32,9 @@ describe('LazyChild', () => {
   function Screen({ offset }: { offset?: number }) {
     return (
       <LazyScrollView offset={offset}>
-        <ChildOne />
-        <ChildTwo />
+        <Child onThresholdPass={onThresholdPassOne} />
+        <Child onThresholdPass={onThresholdPassTwo} />
+        <Child onThresholdPass={onThresholdPassThree} />
       </LazyScrollView>
     );
   }
@@ -50,16 +43,26 @@ describe('LazyChild', () => {
     fireEvent(screen.UNSAFE_queryAllByType(Animated.View)[0], 'layout', {
       nativeEvent: {
         layout: {
-          height: CHILD_HEIGHT,
+          height: HEIGHT,
           y: 0,
         },
       },
     });
+
     fireEvent(screen.UNSAFE_queryAllByType(Animated.View)[1], 'layout', {
       nativeEvent: {
         layout: {
-          height: CHILD_HEIGHT,
-          y: CHILD_HEIGHT + 1,
+          height: HEIGHT,
+          y: HEIGHT,
+        },
+      },
+    });
+
+    fireEvent(screen.UNSAFE_queryAllByType(Animated.View)[2], 'layout', {
+      nativeEvent: {
+        layout: {
+          height: SMALL_HEIGHT,
+          y: HEIGHT * 2,
         },
       },
     });
@@ -67,7 +70,20 @@ describe('LazyChild', () => {
     fireEvent(screen.UNSAFE_queryByType(ScrollView), 'layout', {
       nativeEvent: {
         layout: {
-          height: SCROLLVIEW_HEIGHT,
+          height: HEIGHT,
+        },
+      },
+    });
+  };
+
+  const fireScroll = (toValue: number) => {
+    fireEvent.scroll(screen.UNSAFE_queryByType(ScrollView), {
+      nativeEvent: {
+        contentOffset: {
+          y: toValue,
+        },
+        contentSize: {
+          height: CONTENT_HEIGHT,
         },
       },
     });
@@ -76,6 +92,7 @@ describe('LazyChild', () => {
   afterEach(() => {
     onThresholdPassOne.mockClear();
     onThresholdPassTwo.mockClear();
+    onThresholdPassThree.mockClear();
     cleanup();
   });
 
@@ -90,6 +107,7 @@ describe('LazyChild', () => {
 
     expect(onThresholdPassOne).toHaveBeenCalledTimes(1);
     expect(onThresholdPassTwo).toHaveBeenCalledTimes(0);
+    expect(onThresholdPassThree).toHaveBeenCalledTimes(0);
   });
 
   it('calls onThresholdPass for ChildTwo when threshold is passed', () => {
@@ -97,19 +115,40 @@ describe('LazyChild', () => {
 
     act(() => {
       fireOnLayouts();
-
-      fireEvent.scroll(screen.UNSAFE_queryByType(ScrollView), {
-        nativeEvent: {
-          contentOffset: {
-            y: CHILD_HEIGHT - SCROLLVIEW_HEIGHT + 2,
-          },
-        },
-      });
+      fireScroll(1);
     });
 
     update(<Screen />);
 
     expect(onThresholdPassOne).toHaveBeenCalledTimes(1);
     expect(onThresholdPassTwo).toHaveBeenCalledTimes(1);
+    expect(onThresholdPassThree).toHaveBeenCalledTimes(0);
+  });
+
+  it('calls onThresholdPass for ChildThree when end of scrollview is reached', () => {
+    const aboveThirdChild = -SMALL_HEIGHT - 10;
+    const fullContentScroll = HEIGHT + SMALL_HEIGHT;
+    const { update } = render(<Screen offset={aboveThirdChild} />);
+
+    act(() => {
+      fireOnLayouts();
+      fireScroll(fullContentScroll - 10);
+    });
+
+    update(<Screen />);
+
+    expect(onThresholdPassOne).toHaveBeenCalledTimes(1);
+    expect(onThresholdPassTwo).toHaveBeenCalledTimes(1);
+    expect(onThresholdPassThree).toHaveBeenCalledTimes(0);
+
+    act(() => {
+      fireScroll(fullContentScroll);
+    });
+
+    update(<Screen />);
+
+    expect(onThresholdPassOne).toHaveBeenCalledTimes(1);
+    expect(onThresholdPassTwo).toHaveBeenCalledTimes(1);
+    expect(onThresholdPassThree).toHaveBeenCalledTimes(1);
   });
 });
