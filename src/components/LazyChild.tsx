d0adf329e813c7bd3e6ea0c8ba15d4a1ef16195d
John Haupenthal <johnhaup@gmail.com>
2024-05-12 20:41:04 -0700

    feat: add percent lazy child trigger

--- src/components/LazyChild.tsx
+++ src/components/LazyChild.tsx
@@ -8,32 +8,46 @@ import Animated, {
 } from 'react-native-reanimated';
 import { useAnimatedContext } from '../context/AnimatedContext';
 
-/**
- * @param onThresholdPass - callback to fire when the LazyChild's top position passes the LazyScrollView's offset
- * @returns Wrapper component to wrap your components in.  Components wrapped in LazyChild will trigger their onThresholdPass callback when their top position passes the LazyScrollView's offset
- */
+interface Props {
+  children: React.ReactNode;
+  /**
+   * Callback to fire when the LazyChild's top position passes the LazyScrollView's offset
+   */
+  onThresholdPass: () => void;
+  /**
+   * How much of the LazyChild should be visible before the percent visible threshold is passed.  For example, 0.5 would fire onPercentVisibleThresholdPass when 50% of the LazyChild is visible.  This has no effect if onPercentVisibleThresholdPass is not provided.  Defaults to 1.0.
+   */
+  percentVisibleThreshold?: number;
+  /**
+   * Callback to fire when the LazyChild's viewable area exceeds the percentVisibleThreshold.
+   */
+  onPercentVisibleThresholdPass?: () => void;
+}
 
+/**
+ * Components wrapped in LazyChild will trigger their onThresholdPass callback when their top position passes the LazyScrollView's offset
+ */
 export function LazyChild({
   children,
   onThresholdPass,
-}: {
-  children: React.ReactNode;
-  onThresholdPass: () => void;
-}) {
-  const { triggerValue, hasReachedEnd, scrollValue } = useAnimatedContext();
+  percentVisibleThreshold,
+  onPercentVisibleThresholdPass,
+}: Props) {
+  const { triggerValue, hasReachedEnd, scrollValue, bottomYValue } =
+    useAnimatedContext();
   const _viewRef = useAnimatedRef<Animated.View>();
-  const _hasFiredTrigger = useSharedValue(false);
+  const _hasFiredGlobalTrigger = useSharedValue(false);
 
-  const handleTrigger = useCallback(() => {
-    if (!_hasFiredTrigger.value) {
-      _hasFiredTrigger.value = true;
+  const handleGlobalTrigger = useCallback(() => {
+    if (!_hasFiredGlobalTrigger.value) {
+      _hasFiredGlobalTrigger.value = true;
       onThresholdPass();
     }
-  }, [_hasFiredTrigger, onThresholdPass]);
+  }, [_hasFiredGlobalTrigger, onThresholdPass]);
 
   useAnimatedReaction(
     () => {
-      if (_hasFiredTrigger.value) {
+      if (_hasFiredGlobalTrigger.value) {
         return false;
       }
 
@@ -46,7 +60,8 @@ export function LazyChild({
       // scrollValue only here to make the reaction fire
       if (measurement !== null && scrollValue.value > -1) {
         return (
-          measurement.pageY < triggerValue.value && !_hasFiredTrigger.value
+          measurement.pageY < triggerValue.value &&
+          !_hasFiredGlobalTrigger.value
         );
       }
 
@@ -54,7 +69,63 @@ export function LazyChild({
     },
     (hasPassedThreshold) => {
       if (hasPassedThreshold) {
-        runOnJS(handleTrigger)();
+        runOnJS(handleGlobalTrigger)();
+      }
+    }
+  );
+
+  const _shouldMeasurePercentVisible = useSharedValue(
+    typeof onPercentVisibleThresholdPass === 'function'
+  );
+  const _percentVisibleTrigger = useSharedValue(percentVisibleThreshold || 1);
+  const _hasFiredPercentVisibleTrigger = useSharedValue(false);
+
+  const handlePercentTrigger = useCallback(() => {
+    if (
+      !_hasFiredPercentVisibleTrigger.value &&
+      onPercentVisibleThresholdPass
+    ) {
+      _hasFiredPercentVisibleTrigger.value = true;
+      onPercentVisibleThresholdPass();
+    }
+  }, [_hasFiredPercentVisibleTrigger, onPercentVisibleThresholdPass]);
+
+  useAnimatedReaction(
+    () => {
+      if (!_shouldMeasurePercentVisible) {
+        return false;
+      }
+
+      if (_hasFiredPercentVisibleTrigger.value) {
+        return false;
+      }
+
+      if (hasReachedEnd.value) {
+        return true;
+      }
+
+      const measurement = measure(_viewRef);
+
+      // scrollValue only here to make the reaction fire
+      if (measurement !== null && scrollValue.value > -1) {
+        const percentOffset = measurement.height * _percentVisibleTrigger.value;
+        const percentTrigger = bottomYValue.value - percentOffset;
+
+        if (percentTrigger <= 0) {
+          return false;
+        }
+
+        return (
+          measurement.pageY < percentTrigger &&
+          !_hasFiredPercentVisibleTrigger.value
+        );
+      }
+
+      return false;
+    },
+    (hasPassedThreshold) => {
+      if (hasPassedThreshold) {
+        runOnJS(handlePercentTrigger)();
       }
     }
   );
