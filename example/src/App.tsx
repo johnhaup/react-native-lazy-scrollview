ec5b841528e0b215500c588f0dbdfc246b4f2323
John Haupenthal <johnhaup@gmail.com>
2024-08-04 19:54:42 -0700

    chore: improve example images

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 
 import { random } from 'lodash';
 import shuffle from 'lodash/shuffle';
-import { StyleSheet, Text, View } from 'react-native';
+import compact from 'lodash/compact';
+import {
+  Image,
+  ImageSourcePropType,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import { LazyScrollView } from 'react-native-lazy-scrollview';
 import { ColorBlock } from './components/ColorBlock';
 
-const ALBUMS = [
-  'https://audioxide.com/api/images/album-artwork/in-utero-nirvana-medium-square.jpg',
-  'https://audioxide.com/api/images/album-artwork/pinkerton-weezer-medium-square.jpg',
-  'https://static1.squarespace.com/static/55b7b1bde4b0828f7d1438de/t/6091766fa1c5b50917511c4b/1620145812302/WAVVES+-+Hideaway+-+Album+Cover+3000x3000.jpg?format=1500w',
-  'https://i.scdn.co/image/ab67616d0000b27313f2466b83507515291acce4',
-  'https://m.media-amazon.com/images/I/515NY3NS1EL._UF1000,1000_QL80_.jpg',
-  'https://images.squarespace-cdn.com/content/v1/5e270e203c421657bd3e3208/1584536470858-1OSWN7WUQ3DY749LGHOU/R-1843384-1282324049.jpeg.jpg',
+const ALBUMS: ImageSourcePropType = [
+  require('../assets/albums/american-psycho.jpg'),
+  require('../assets/albums/doggstyle.jpg'),
+  require('../assets/albums/dude-ranch.jpg'),
+  require('../assets/albums/in_utero.jpg'),
+  require('../assets/albums/is-this-it.jpg'),
+  require('../assets/albums/let-it-be.jpg'),
+  require('../assets/albums/rip-this.jpeg'),
+  require('../assets/albums/spilt-milk.jpg'),
+  require('../assets/albums/suffer.jpg'),
+  require('../assets/albums/t-hives.jpg'),
+  require('../assets/albums/trendkill.jpg'),
+  require('../assets/albums/wysiatwin.jpg'),
+  require('../assets/albums/youre-welcome.jpeg'),
   null,
-  'https://m.media-amazon.com/images/I/61vMlYT58HL._UF1000,1000_QL80_.jpg',
-  'https://e.snmc.io/i/1200/s/9a0f51b8b776171aaee65c1a352f6d11/2396429',
-  'https://global-uploads.webflow.com/5fda4244c42e015b06d2fd8d/5fdbca8f15a8ee0f0f369767_cover.jpg',
-  'https://i.discogs.com/GXyIsX5gBpgw3XMtEZ-FvyVJBDZyk7Aoq-bPzV_yIXk/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTIyNDY1/MTUtMTI5OTE5MjI1/NS5qcGVn.jpeg',
   null,
 ];
 
@@ -25,8 +35,11 @@ const OFFSET = -100;
 const SHUFFLED_ALBUMS = shuffle(ALBUMS);
 
 export default function App() {
-  const renderBlock = (uri: string | null, i: number) => (
-    <ColorBlock key={`child_${i}`} uri={uri} nested={random(1) === 1} />
+  const renderBlock = useCallback(
+    (source: ImageSourcePropType | null, i: number) => (
+      <ColorBlock key={`child_${i}`} source={source} nested={random(1) === 1} />
+    ),
+    []
   );
 
   return (
