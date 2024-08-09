70c7ee15925a3a6412d61a75bbebe21a621f1190
John Haupenthal <johnhaup@gmail.com>
2024-08-08 20:30:32 -0700

    chore: remove shared values from hook deps

--- src/components/LazyChild.tsx
+++ src/components/LazyChild.tsx
@@ -59,7 +59,8 @@ export function LazyChild({
       _hasFiredScrollViewThresholdTrigger.value = true;
       onThresholdPass();
     }
-  }, [_hasFiredScrollViewThresholdTrigger, onThresholdPass]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps -- shared values do not trigger re-renders
+  }, []);
 
   useAnimatedReaction(
     () => {
@@ -69,10 +70,6 @@ export function LazyChild({
 
       if (hasReachedEnd.value) {
         return true;
-      }
-
-      if (!_viewRef) {
-        return false;
       }
 
       if (_canMeasure.value) {
@@ -169,7 +166,7 @@ export function LazyChild({
     }
 
     return false;
-  }, []);
+  });
 
   useAnimatedReaction(
     () => isVisible.value,
@@ -186,17 +183,14 @@ export function LazyChild({
     }
   );
 
-  const onLayout = useCallback(
-    ({ nativeEvent }: LayoutChangeEvent) => {
-      // Don't measure until we know we have something.  This prevents those pesky Android measurement warnings.
-      // https://github.com/software-mansion/react-native-reanimated/blob/d8ef9c27c31dd2c32d4c3a2111326a448bf19ec9/packages/react-native-reanimated/src/platformFunctions/measure.ts#L95
-      _canMeasure.value = nativeEvent.layout.height > 0;
-    },
-    [_canMeasure]
-  );
+  const onLayout = useCallback(({ nativeEvent }: LayoutChangeEvent) => {
+    // Don't measure until we know we have something.  This prevents those pesky Android measurement warnings.
+    // https://github.com/software-mansion/react-native-reanimated/blob/d8ef9c27c31dd2c32d4c3a2111326a448bf19ec9/packages/react-native-reanimated/src/platformFunctions/measure.ts#L95
+    _canMeasure.value = nativeEvent.layout.height > 0;
+    // eslint-disable-next-line react-hooks/exhaustive-deps -- shared values do not trigger re-renders
+  }, []);
 
   return (
-    // https://github.com/software-mansion/react-native-reanimated/blob/d8ef9c27c31dd2c32d4c3a2111326a448bf19ec9/packages/react-native-reanimated/src/platformFunctions/measure.ts#L56
     <Animated.View ref={_viewRef} onLayout={onLayout}>
       {children}
     </Animated.View>
