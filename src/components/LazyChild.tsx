7d3a3d04f9908aa9d71e9f27bfc1ca67e4eb358f
John Haupenthal <johnhaup@gmail.com>
2024-08-08 20:01:38 -0700

    fix: validate component has height before measuring

--- src/components/LazyChild.tsx
+++ src/components/LazyChild.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback } from 'react';
+import type { LayoutChangeEvent } from 'react-native';
 import Animated, {
   measure,
   runOnJS,
@@ -48,6 +49,7 @@ export function LazyChild({
   const { triggerValue, hasReachedEnd, scrollValue, topYValue, bottomYValue } =
     useAnimatedContext();
 
+  const _canMeasure = useSharedValue(false);
   const _viewRef = useAnimatedRef<Animated.View>();
   const _hasFiredScrollViewThresholdTrigger = useSharedValue(false);
   const _ignoreZeroMeasurement = useSharedValue(ignoreZeroMeasurement);
@@ -69,18 +71,24 @@ export function LazyChild({
         return true;
       }
 
-      const measurement = measure(_viewRef);
+      if (!_viewRef) {
+        return false;
+      }
 
-      // Track scollValue to make reaction fire
-      if (measurement !== null && scrollValue.value > -1) {
-        if (_ignoreZeroMeasurement.value && measurement.pageY === 0) {
-          return false;
+      if (_canMeasure.value) {
+        const measurement = measure(_viewRef);
+
+        // Track scollValue to make reaction fire
+        if (measurement !== null && scrollValue.value > -1) {
+          if (_ignoreZeroMeasurement.value && measurement.pageY === 0) {
+            return false;
+          }
+
+          return (
+            measurement.pageY < triggerValue.value &&
+            !_hasFiredScrollViewThresholdTrigger.value
+          );
         }
-
-        return (
-          measurement.pageY < triggerValue.value &&
-          !_hasFiredScrollViewThresholdTrigger.value
-        );
       }
 
       return false;
@@ -132,34 +140,36 @@ export function LazyChild({
 
   const isVisible = useDerivedValue(() => {
     if (_WORKLET) {
-      const measurement = measure(_viewRef);
+      if (_canMeasure.value) {
+        const measurement = measure(_viewRef);
 
-      // Track scollValue to make reaction fire
-      if (measurement !== null && scrollValue.value > -1) {
-        const topOfView = measurement.pageY;
-        const bottomOfView = measurement.pageY + measurement.height;
+        // Track scollValue to make reaction fire
+        if (measurement !== null && scrollValue.value > -1) {
+          const topOfView = measurement.pageY;
+          const bottomOfView = measurement.pageY + measurement.height;
 
-        if (_ignoreZeroMeasurement.value && topOfView === 0) {
-          return false;
+          if (_ignoreZeroMeasurement.value && topOfView === 0) {
+            return false;
+          }
+
+          const visibilityHeight =
+            measurement.height * _percentVisibleTrigger.value;
+          const visibleEnterTrigger = bottomYValue.value - visibilityHeight;
+          const visibleExitTrigger = topYValue.value + visibilityHeight;
+
+          if (visibleEnterTrigger <= 0) {
+            return false;
+          }
+
+          return (
+            topOfView < visibleEnterTrigger && bottomOfView > visibleExitTrigger
+          );
         }
-
-        const visibilityHeight =
-          measurement.height * _percentVisibleTrigger.value;
-        const visibleEnterTrigger = bottomYValue.value - visibilityHeight;
-        const visibleExitTrigger = topYValue.value + visibilityHeight;
-
-        if (visibleEnterTrigger <= 0) {
-          return false;
-        }
-
-        return (
-          topOfView < visibleEnterTrigger && bottomOfView > visibleExitTrigger
-        );
       }
     }
 
     return false;
-  });
+  }, []);
 
   useAnimatedReaction(
     () => isVisible.value,
@@ -176,9 +186,18 @@ export function LazyChild({
     }
   );
 
+  const onLayout = useCallback(
+    ({ nativeEvent }: LayoutChangeEvent) => {
+      // Don't measure until we know we have something.  This prevents those pesky Android measurement warnings.
+      // https://github.com/software-mansion/react-native-reanimated/blob/d8ef9c27c31dd2c32d4c3a2111326a448bf19ec9/packages/react-native-reanimated/src/platformFunctions/measure.ts#L95
+      _canMeasure.value = nativeEvent.layout.height > 0;
+    },
+    [_canMeasure]
+  );
+
   return (
     // https://github.com/software-mansion/react-native-reanimated/blob/d8ef9c27c31dd2c32d4c3a2111326a448bf19ec9/packages/react-native-reanimated/src/platformFunctions/measure.ts#L56
-    <Animated.View ref={_viewRef} collapsable={false}>
+    <Animated.View ref={_viewRef} onLayout={onLayout}>
       {children}
     </Animated.View>
   );
